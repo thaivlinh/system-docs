75e7b5c63205d9c46dca62727288da6e01bd4282
Thái Việt Linh <linhtv@greengroups.vn>
2023-04-17 16:55:42 +0700

    update: url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://thaivlinh.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/system-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thaivlinh', // Usually your GitHub org/user name.
   projectName: 'system-docs', // Usually your repo name.
   deploymentBranch: 'docs',
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
